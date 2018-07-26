faa6e75c27321a1f68e0880cc4dee844d6e4e4c6
tragic86 <oneil5@aol.com>
2018-07-26 10:15:40 +0000

    create a file

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+#script to create file in puppet
+
+
+
+file {'/tmp/holberton':
+path    => '/tmp/holberton',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0744',
+content => 'I love Puppet',
+}
